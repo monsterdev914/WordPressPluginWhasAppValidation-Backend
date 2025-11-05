d2690ffc30fb58fb4d8efabb2572aafe9948056e
skytech2510 <skytech2510@gmail.com>
2025-11-06 04:36:50 +0900

    chore: udpate

--- app.py
+++ app.py
@@ -91,7 +91,3 @@ def add_account(new_account: Account):
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
